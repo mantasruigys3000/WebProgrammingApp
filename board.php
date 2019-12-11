7a30e5813a573b4058844f26e7f3c53d26a41f06
Teodor <mice.graff@gmail.com>
2019-12-11 12:08:23 +0000

    Full Website
    This version works

--- board.php
+++ board.php
@@ -1,18 +1,16 @@
 <?php
     session_start();
+
+    $url = "$_SERVER[REQUEST_URI]" . "?";
+
     if(!isset($_GET['page'])){
         $_GET['page'] =1 ;
-
     }
 
     $maxCards = 11;
 
-    
-
-
     require './db.php';
     $db = new db();
-
     $company_list = $db->getCompanies(
         count($_GET),
         $_GET['search'],
@@ -21,19 +19,31 @@
         $_GET['startrange'],
         $_GET['endrange'],
         ($_GET['page'] -1) * $maxCards,
-        (($_GET['page'] -1) * $maxCards) + $maxCards
-
-
+        $maxCards
     );
 
-
-    
+    $company_count = $db->getCompanyCount();
+    $page_amount = ceil($company_count/$maxCards);
 
     if ( !isset( $_SESSION['username'] ) ) {
         // Redirect them to the login page
         header("Location: index.php");
     }
 
+    $get_array = array($_GET);
+
+    $url = "board.php?";
+    $parameters;
+
+    foreach ($get_array as $get_var){
+        unset($get_var['page']);
+        foreach ($get_var as $key => $value) {
+            $new_par = $key . "=" . $value . "&";
+            $parameters .= $new_par;
+        }
+    }
+
+    $url .= str_replace(" ", "+", $parameters);
 ?>
 
 <!DOCTYPE html>
@@ -72,6 +82,7 @@
             <form style="margin: auto; width: 80%;" action="board.php" method="GET">
                 <div class="input-group mb-3">
                     <div class="input-group-prepend">
+                        <!-- Its crucial that the type is button so the page doesnt refresh on click -->
                         <button class="button input-group-text outline-primary " type="button" id="basic-addon3" onclick="toggleAdvSearch()" > Adv. Search </button>
                     </div>
                     <input type="text" class="form-control text-center" placeholder="Try searching for a company!" name="search">
@@ -97,10 +108,10 @@
                         <div style="padding-top: 1.1%;">CATEGORY</div>
                         <div class="input-group" style="width: 100%;" >
                             <select class="custom-select" id="inputSelectSearchFilter" name="type">
-                                <option id="se" selected>Software Engineering</option>
+                                <option value="" selected>All</option>
+                                <option value="Software Engineering">Software Engineering</option>
                                 <option value="Computing">Computing</option>
                                 <option value="Data">Data</option>
-                                <option value="All">All</option>
                             </select>
                         </div>
                     </div>
@@ -108,6 +119,7 @@
             </form>
     </div>
 
+    <!-- The toggle for Adv. Search -->
     <script>
         function toggleAdvSearch(){
             var x = document.getElementById("collapse1");
@@ -124,7 +136,7 @@
     <div class="main-body">
         <div id="edit-modal" class="modal-custom">
         <!-- Modal content -->
-            <div class="card" style="width: 75%; height: 75%; margin: 7% auto;">
+            <div class="card" style="width: 75%; height: 75%; margin: 7% auto; overflow:scroll;">
                 <div class="card-header ">
                     Company Information
                     <span class="close">&times;</span>
@@ -140,7 +152,7 @@
                             <h5 class="text-uppercase font-weight-bold text-center" style="width: 90%; margin:auto; margin-top: 10%;">company email</h5>
                             <p id="modal-company-email" class="text-center border-bottom" style="width: 90%; margin:auto;">yeah</p>
                         </div>
-                        <div id="black-bar" style="height: 350px; width: 1px; background-color:#e6e6e6; margin-top: 9%; margin-left: 5%;"></div>
+
                         <div class="col-md-7 w-100">
                             <h5 id="modal-company-name" class="card-title text-uppercase font-weight-bold text-center border-bottom" style="width: 80%; margin:auto;">Intel</h5>
                             <p id="modal-company-desc" class="card-text border-bottom p-4 " >Intel Corporation is an American multinational corporation and technology company headquartered in Santa Clara, California.</p>
@@ -209,6 +221,28 @@
             </div>
         </div>
 
+        
+        <div class="container-fluid " >
+        <!-- Pagination Top-->
+        <div class= "row " id="display-settings" >
+                <div class='col' style="text-align: center;" >
+                        <nav aria-label="Page navigation"style="display: inline-block;"  >
+                            <ul class="pagination" >
+                                <?php
+                                for ($x = 1; $x <= $page_amount; $x++) {
+                                    $page_button = sprintf("
+                                    <li class='page-item'>
+                                        <a class='page-link' href='%spage=%s'>%s</a>
+                                    </li>", $url, $x, $x);
+                                    echo($page_button);
+                                }
+                                ?>
+                            </ul>
+                        </nav>
+                </div>
+            </div>
+        </div>
+
         <div class= "container-fluid mt-4" >
         <!-- Card Generator + Add Card -->
             <div class= "row justify-content-center" id="company-block-row">
@@ -243,12 +277,44 @@
                 ?>
         </div>
 
+        <div class="container-fluid mt-3" >
+        <!-- Pagination Bottom-->
+        <div class= "row " id="display-settings" >
+                <div class='col' style="text-align: center;" >
+                        <nav aria-label="Page navigation"style="display: inline-block;"  >
+                            <ul class="pagination" >
+                                <?php
+                                for ($x = 1; $x <= $page_amount; $x++) {
+                                    $page_button = sprintf("
+                                    <li class='page-item'>
+                                        <a class='page-link' href='%spage=%s'>%s</a>
+                                    </li>", $url, $x, $x);
+                                    echo($page_button);
+                                }
+                                ?>
+                            </ul>
+                        </nav>
+                </div>
+            </div>
+        </div>
+
         <div class="footer" style="margin-top:80px" >
         <!-- Footer -->
-            <div class="card-footer bg-primary border-primary fixed-bottom" style="margin-top:10px;">
-                <form method="post" action="logout.php">
-                <button type="submit" class="btn btn-primary text-uppercase font-weight-bold">Log out</button>
-                </form>
+            <div class="row card-footer bg-primary border-primary fixed-bottom" style="margin-top:10px;">
+                <div class="col mr-auto">
+                    <div class="float-left">
+                        <form method="post" action="logout.php">
+                            <button type="submit" class="col btn btn-primary text-uppercase font-weight-bold">Log out</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col ml-auto">
+                    <div class="float-right">
+                        <form method="post" action="board-guest.php">
+                            <button type="submit" class="btn btn-primary text-uppercase font-weight-bold">View as Guest</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
